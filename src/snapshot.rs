bd853a330d1fd15ef1934e5e3a978fbfdbf87323
Samuel Schlesinger <samuelschlesinger@Samuels-MacBook-Air.local>
2023-04-28 19:41:45 -0400

    boop

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -2,9 +2,13 @@ use serde::{Deserialize, Serialize};
 
 use crate::object_id::ObjectId;
 
+/// A particular snapshot of a version.
 #[derive(PartialEq, Eq, Debug, Clone, Serialize, Deserialize)]
 pub struct SnapShot {
+    /// The [`ObjectId`] of the message added with the commit.
     pub message: ObjectId,
+    /// The [`ObjectId`] of the directory structure.
     pub directory: ObjectId,
-    pub previous: ObjectId,
+    /// The [`ObjectId`] of the previous snapshot, if there was one.
+    pub previous: Option<ObjectId>,
 }
